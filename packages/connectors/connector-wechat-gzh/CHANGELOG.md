69db7d3802ea2c96cb03b8f1cf0da8c4b3472a0b
zhuang <1041036986@qq.com>
2024-09-17 19:28:15 +0800

    feat: add wechat gzh connector

---
+++ packages/connectors/connector-wechat-gzh/CHANGELOG.md
@@ -0,0 +1,8 @@
+# @logto/connector-wechat-gzh
+
+## 0.0.1
+
+### Minor Changes
+
+- 5581f6476: enable configurable `scope` (#3723)
+  
---
+++ packages/connectors/connector-wechat-gzh/src/types.ts
@@ -0,0 +1,45 @@
+import { z } from 'zod';
+
+export const wechatConfigGuard = z.object({
+  appId: z.string(),
+  appSecret: z.string(),
+  scope: z.string().optional(),
+});
+
+export type WechatConfig = z.infer<typeof wechatConfigGuard>;
+
+export const accessTokenResponseGuard = z.object({
+  access_token: z.string().optional(),
+  expires_in: z.number().optional(), // In seconds
+  refresh_token: z.string().optional(),
+  openid: z.string().optional(),
+  scope: z.string().optional(),
+  is_snapshotuser: z.number().optional(),
+  unionid: z.string().optional(),
+  errcode: z.number().optional(),
+  errmsg: z.string().optional(),
+});
+
+export type AccessTokenResponse = z.infer<typeof accessTokenResponseGuard>;
+
+export type GetAccessTokenErrorHandler = (accessToken: Partial<AccessTokenResponse>) => void;
+
+export const userInfoResponseGuard = z.object({
+  openid: z.string().optional(),
+  nickname: z.string().optional(),
+  sex: z.number().optional(),
+  province: z.string().optional(),
+  city: z.string().optional(),
+  country: z.string().optional(),
+  headimgurl: z.string().optional(),
+  unionid: z.string().optional(),
+  privilege: z.array(z.string()).optional(),
+  errcode: z.number().optional(),
+  errmsg: z.string().optional(),
+});
+
+export type UserInfoResponse = z.infer<typeof userInfoResponseGuard>;
+
+export type UserInfoResponseMessageParser = (userInfo: Partial<UserInfoResponse>) => void;
+
+export const authResponseGuard = z.object({ code: z.string(), state: z.string() });
